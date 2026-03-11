cc6c846cfd473a2e91aa2595a75f4608a84b3301
Pedro <th851627@gmail.com>
2026-03-11 02:44:56 -0600

    Protect write routes with verifyToken middleware

--- src/routes/offersRoutes.js
+++ src/routes/offersRoutes.js
@@ -3,11 +3,12 @@ import {
   getOffers,
   createOffer
 } from '../controllers/offersController.js'
+import { verifyToken } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
 router.get('/offers', getOffers)
 
-router.post('/offers', createOffer)
+router.post('/offers', verifyToken, createOffer)
 
 export default router
