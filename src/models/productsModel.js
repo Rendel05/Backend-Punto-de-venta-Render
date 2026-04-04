a4cb18a35352b315572810a0a32c5b9f7060d3b0
Rendel05 <th851627@gmail.com>
2026-04-03 20:34:28 -0600

    Refactor(products): refactor in products model, now using a view

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -2,15 +2,8 @@ import db from '../config/db.js'
 
 export const getAllProducts = async (page, limit) => {
   const offset = (page - 1) * limit
-
-  const [rows] = await db.query(
-    "SELECT * FROM productos LIMIT ? OFFSET ?",
-    [limit, offset]
-  )
-
-  const [[{ total }]] = await db.query(
-    "SELECT COUNT(*) as total FROM productos"
-  )
+  const [rows] = await db.query("SELECT * FROM productos LIMIT ? OFFSET ?", [limit, offset])
+  const [[{ total }]] = await db.query("SELECT COUNT(*) as total FROM productos")
 
   return {
     data: rows,
@@ -20,45 +13,18 @@ export const getAllProducts = async (page, limit) => {
   }
 }
 
+
 export const getAllActiveProducts = async (page, limit) => {
   const offset = (page - 1) * limit;
 
   const [rows] = await db.query(
-    `SELECT 
-        p.*, 
-        c.nombre AS categoria_nombre,
-
-        o.precio_oferta,
-
-        CASE 
-          WHEN o.oferta_id IS NOT NULL THEN 1
-          ELSE 0
-        END AS tiene_oferta,
-
-        CASE 
-          WHEN o.oferta_id IS NOT NULL THEN o.precio_oferta
-          ELSE p.precio_venta
-        END AS precio_final
-
-     FROM productos p
-
-     INNER JOIN categorias c 
-        ON p.categoria_id = c.categoria_id
-
-     LEFT JOIN ofertas o 
-        ON p.producto_id = o.producto_id
-        AND o.activo = 1
-        AND CURDATE() BETWEEN o.fecha_inicio AND o.fecha_fin
-
-     WHERE p.activo = 1
-
+    `SELECT * FROM vw_productos_con_oferta 
+     WHERE activo = 1 
      LIMIT ? OFFSET ?`,
     [limit, offset]
   );
 
-  const [[{ total }]] = await db.query(
-    "SELECT COUNT(*) as total FROM productos WHERE activo = 1"
-  )
+  const [[{ total }]] = await db.query("SELECT COUNT(*) as total FROM productos WHERE activo = 1")
 
   return {
     data: rows,
@@ -72,35 +38,8 @@ export const getProductByCategory = async (page, limit, category) => {
   const offset = (page - 1) * limit;
 
   const [rows] = await db.query(
-    `SELECT 
-        p.*, 
-        c.nombre AS categoria_nombre,
-
-        o.precio_oferta,
-
-        CASE 
-          WHEN o.oferta_id IS NOT NULL THEN 1
-          ELSE 0
-        END AS tiene_oferta,
-
-        CASE 
-          WHEN o.oferta_id IS NOT NULL THEN o.precio_oferta
-          ELSE p.precio_venta
-        END AS precio_final
-
-     FROM productos p
-
-     INNER JOIN categorias c 
-        ON p.categoria_id = c.categoria_id
-
-     LEFT JOIN ofertas o 
-        ON p.producto_id = o.producto_id
-        AND o.activo = 1
-        AND CURDATE() BETWEEN o.fecha_inicio AND o.fecha_fin
-
-     WHERE p.categoria_id = ?
-     AND p.activo = 1
-
+    `SELECT * FROM vw_productos_con_oferta 
+     WHERE categoria_id = ? AND activo = 1 
      LIMIT ? OFFSET ?`,
     [category, limit, offset]
   )
@@ -120,187 +59,76 @@ export const getProductByCategory = async (page, limit, category) => {
 
 export const getProductById = async (id) => {
   const [rows] = await db.query(
-    `SELECT 
-        p.*, 
-        c.nombre AS categoria_nombre,
-
-        o.precio_oferta,
-
-        CASE 
-          WHEN o.oferta_id IS NOT NULL THEN 1
-          ELSE 0
-        END AS tiene_oferta,
-
-        CASE 
-          WHEN o.oferta_id IS NOT NULL THEN o.precio_oferta
-          ELSE p.precio_venta
-        END AS precio_final
-
-     FROM productos p
-
-     INNER JOIN categorias c 
-        ON p.categoria_id = c.categoria_id
-
-     LEFT JOIN ofertas o 
-        ON p.producto_id = o.producto_id
-        AND o.activo = 1
-        AND CURDATE() BETWEEN o.fecha_inicio AND o.fecha_fin
-
-     WHERE p.producto_id = ?
-     AND p.activo = 1`,
+    `SELECT * FROM vw_productos_con_oferta 
+     WHERE producto_id = ? AND activo = 1`,
     [id]
   )
-  return rows[0]
+  return rows
 }
-
-export const addProduct = async (name,code,description,buy_price,sell_price,stock,cat_id,supp_id,status,image_id,public_image_id) => {
-
-    const [result] = await db.query(
-        `INSERT INTO productos
-        (nombre, codigo, descripcion, precio_compra, precio_venta,
-        stock, categoria_id, proveedor_id, fecha_creacion,
-        activo, imagen_url, imagen_public_id)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, CURDATE(), ?, ?, ?)`,
-        [
-        name,
-        code,
-        description,
-        buy_price,
-        sell_price,
-        stock,
-        cat_id,
-        supp_id,
-        status,
-        image_id,
-        public_image_id
-        ]
-    )
-    return result.affectedRows
-}
-
-export const updateProduct = async(id,name,code,description,buy_price,sell_price,stock,cat_id,supp_id,status,image_id,public_image_id) => {
-    const [result] = await db.query(
-        `UPDATE productos SET nombre = ?, codigo = ?, descripcion = ?, precio_compra = ?, precio_venta= ?,stock =?,categoria_id = ?,proveedor_id=?,activo=?,imagen_url=?,imagen_public_id=? WHERE producto_id = ?`
-        ,[name,code,description,buy_price,sell_price,stock,cat_id,supp_id,status,image_id,public_image_id,id]
-    )
-    return result.affectedRows
-}
-
-export const updateProductStatus = async (id, estado) => {
-    const [result] = await db.query(
-        `UPDATE productos SET activo = ? WHERE producto_id = ?`,
-        [estado, id]
-    )
-
-    return result.affectedRows;
-}
-
 
 export const searchProducts = async (search, page, limit) => {
   const searchTerm = `%${search}%`;
   const offset = (page - 1) * limit;
 
   const [rows] = await db.query(
-    `SELECT 
-        p.*, 
-        c.nombre AS categoria,
-
-        o.precio_oferta,
-
-        CASE 
-          WHEN o.oferta_id IS NOT NULL THEN 1
-          ELSE 0
-        END AS tiene_oferta,
-
-        CASE 
-          WHEN o.oferta_id IS NOT NULL THEN o.precio_oferta
-          ELSE p.precio_venta
-        END AS precio_final
-
-     FROM productos p
-
-     INNER JOIN categorias c 
-        ON p.categoria_id = c.categoria_id
-
-     LEFT JOIN ofertas o 
-        ON p.producto_id = o.producto_id
-        AND o.activo = 1
-        AND CURDATE() BETWEEN o.fecha_inicio AND o.fecha_fin
-
-     WHERE (
-        p.nombre LIKE ?
-        OR p.descripcion LIKE ?
-        OR c.nombre LIKE ?
-     )
-     AND p.activo = 1
-
-     ORDER BY p.nombre ASC
-
+    `SELECT * FROM vw_productos_con_oferta 
+     WHERE (nombre LIKE ? OR descripcion LIKE ? OR categoria_nombre LIKE ?)
+     AND activo = 1
+     ORDER BY nombre ASC
      LIMIT ? OFFSET ?`,
     [searchTerm, searchTerm, searchTerm, limit, offset]
   );
 
-  const [count] = await db.query(
-    `SELECT COUNT(*) as total
-     FROM productos p
-     INNER JOIN categorias c 
-        ON p.categoria_id = c.categoria_id
-     WHERE (
-        p.nombre LIKE ?
-        OR p.descripcion LIKE ?
-        OR c.nombre LIKE ?
-     )
-     AND p.activo = 1`,
+  const [[{ total }]] = await db.query(
+    `SELECT COUNT(*) as total FROM vw_productos_con_oferta 
+     WHERE (nombre LIKE ? OR descripcion LIKE ? OR categoria_nombre LIKE ?)
+     AND activo = 1`,
     [searchTerm, searchTerm, searchTerm]
   )
 
-  const total = count[0].total;
-  const totalPages = Math.ceil(total / limit);
-
   return {
     data: rows,
-    totalPages
+    totalPages: Math.ceil(total / limit)
   }
 }
-
 
 export const getPopularProducts = async () => {
   const query = `
     SELECT 
-        p.*, 
-        COUNT(dv.producto_id) AS totalVentas,
-
-        MAX(o.precio_oferta) AS precio_oferta,
-
-        CASE 
-          WHEN MAX(o.oferta_id) IS NOT NULL THEN 1
-          ELSE 0
-        END AS tiene_oferta,
-
-        CASE 
-          WHEN MAX(o.oferta_id) IS NOT NULL THEN MAX(o.precio_oferta)
-          ELSE p.precio_venta
-        END AS precio_final
-
-    FROM productos p
-
-    LEFT JOIN detalle_ventas dv 
-        ON dv.producto_id = p.producto_id
-
-    LEFT JOIN ofertas o 
-        ON p.producto_id = o.producto_id
-        AND o.activo = 1
-        AND CURDATE() BETWEEN o.fecha_inicio AND o.fecha_fin
-
-    WHERE p.activo = 1
-
-    GROUP BY p.producto_id
-
+        v.*, 
+        COUNT(dv.producto_id) AS totalVentas
+    FROM vw_productos_con_oferta v
+    LEFT JOIN detalle_ventas dv ON dv.producto_id = v.producto_id
+    WHERE v.activo = 1
+    GROUP BY v.producto_id
     ORDER BY totalVentas DESC
-
-    LIMIT 10
-  `;
+    LIMIT 10`;
 
   const [rows] = await db.query(query);
   return rows;
+}
+
+export const addProduct = async (name,code,description,buy_price,sell_price,stock,cat_id,supp_id,status,image_id,public_image_id) => {
+    const [result] = await db.query(
+        `INSERT INTO productos
+        (nombre, codigo, descripcion, precio_compra, precio_venta,
+        stock, categoria_id, proveedor_id, fecha_creacion,
+        activo, imagen_url, imagen_public_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, CURDATE(), ?, ?, ?)`,
+        [name, code, description, buy_price, sell_price, stock, cat_id, supp_id, status, image_id, public_image_id]
+    )
+    return result.affectedRows
+}
+
+export const updateProduct = async(id,name,code,description,buy_price,sell_price,stock,cat_id,supp_id,status,image_id,public_image_id) => {
+    const [result] = await db.query(
+        `UPDATE productos SET nombre = ?, codigo = ?, descripcion = ?, precio_compra = ?, precio_venta= ?,stock =?,categoria_id = ?,proveedor_id=?,activo=?,imagen_url=?,imagen_public_id=? WHERE producto_id = ?`,
+        [name,code,description,buy_price,sell_price,stock,cat_id,supp_id,status,image_id,public_image_id,id]
+    )
+    return result.affectedRows
+}
+
+export const updateProductStatus = async (id, estado) => {
+    const [result] = await db.query(`UPDATE productos SET activo = ? WHERE producto_id = ?`, [estado, id])
+    return result.affectedRows;
 }
