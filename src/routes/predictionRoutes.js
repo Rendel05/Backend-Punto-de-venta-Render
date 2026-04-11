0934605a3b92189eedde118e00dd61c1d1e16b5f
Rendel05 <th851627@gmail.com>
2026-04-11 02:19:49 -0600

    Fix: added middleware validation in prediction routes

--- src/routes/predictionRoutes.js
+++ src/routes/predictionRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { getSummary, getPrediction } from '../controllers/predictionController.js';
+import { verifyToken,authorizeRoles } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/prediction/resumen', getSummary);
-router.post('/prediction', getPrediction);
+router.get('/prediction/resumen',verifyToken,authorizeRoles('Admin'), getSummary);
+router.post('/prediction',verifyToken,authorizeRoles('Admin'), getPrediction);
 
 export default router;
